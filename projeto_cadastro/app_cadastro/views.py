c416650cb8ae2d6f047316fbc7a44a6e51d0df7e
LaisPerini <laisperini2@gmail.com>
2023-05-31 21:00:44 -0300

    mensagem feita

--- projeto_cadastro/app_cadastro/views.py
+++ projeto_cadastro/app_cadastro/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def home(request):
     return render(request,'produtos/home.html')
+
+def produtos(request):
+   novo_produto = Produto()
+   novo_produto.nome = request.POST.get('nome')  #request ele visualiza e ele chama o metodo post no html
+   novo_produto.descricao = request.POST.get('descricao')   
+   novo_produto.preco = request.POST.get('preco')  
+   novo_produto.validade = request.POST.get('validade')
+
+   novo_produto.save()                              #salva no banco de dados
+
+   produtos = dict(
+      produtos= Produto.objects.all()    #ele pega todos 
+   )
+
+   return render(request,'produtos/produtos.html',produtos)  
--- projeto_cadastro/projeto_cadastro/urls.py
+++ projeto_cadastro/projeto_cadastro/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from app_cadastro import views
 
 urlpatterns = [
-    # github.com                          url principal
-    #rota ------ github.com/LaisPerini       rota
+    # github.com                 url principal
+    #github.com/LaisPerini       rota
 
-    
     #     rota,view,nome de referencia 
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('produtos/',views.produtos,name='lista_produtos')
 ]
